28ed1ff43d34fb8087b943bb93e854c7597e84b4
manhhungpc <phamhung2002@gmail.com>
2023-04-06 23:59:00 +0700

    update tailwind-css

--- src/routes/admin/costume/+page.svelte
+++ src/routes/admin/costume/+page.svelte
@@ -25,16 +25,21 @@
 
 	function uploadQrLocal(e) {
 		convertToBase64(e, (response) => {
+			if (response.err) {
+				err = response.err;
+				return;
+			}
+			err = null;
 			qr_image = response;
 		});
-		console.log(qr_image);
-		// if (qr_image.err) {
-		// 	console.log(err);
-		// }
 	}
 
 	async function createNewCostume() {
 		loading = true;
+		if (!name || !description || !qr_image || err) {
+			err = 'Có thông tin bị bỏ trống!';
+			return;
+		}
 		const res = await fetch('/api/admin/costume', {
 			method: 'POST',
 			body: JSON.stringify({
@@ -54,33 +59,42 @@
 	}
 </script>
 
-<h1>Admin page, required login</h1>
+<h1 class="text-4xl">Trang dành cho admin</h1>
+<hr />
+<h3 class="text-2xl mt-3">Thêm trang phục mới</h3>
 <div id="new-costume-form">
-	<input placeholder="name" bind:value={name} />
-	<input placeholder="description" bind:value={description} />
+	<input class="text-input" placeholder="Tên trang phục" bind:value={name} required />
+	<input class="text-input" placeholder="Mô tả về trang phục" bind:value={description} required />
 	<div>
-		<label for="qr_image">QR image: </label>
-		<input id="qr_image" type="file" accept="image/*" on:change={uploadQrLocal} />
+		<label for="qr_image">Hình ảnh mã QR: </label>
+		<input id="qr_image" type="file" accept="image/*" on:change={uploadQrLocal} required />
 	</div>
 	{#if qr_image}
 		<img src={qr_image} alt="QR" width="300px" />
 	{/if}
-	{#if err}
-		<p color="#ff0000">Lỗi: {err}</p>
-	{/if}
 	{#if loading}
 		<p>Creating ...</p>
 	{/if}
+	<div class="my-3">
+		<i class="fa-solid fa-arrow-up-right-from-square" />
+		<a href="https://studio.armedia.it/login" class="costume-link">Vào chỉnh sửa hoặc tạo model</a>
+	</div>
+	{#if err}
+		<p class="text-red-600">Lỗi: {err}</p>
+	{/if}
 	<button on:click={createNewCostume}>Create new costume</button>
-	<a href="https://studio.armedia.it/login">Vào chỉnh sửa hoặc tạo model</a>
 </div>
-<h3>List of costume, click to view or edit</h3>
+<hr />
+<h3 class="text-2xl mt-3">Danh sách trang phục hiển thị, ấn vào để chỉnh sửa hoặc xem chi tiết</h3>
 {#await loadData}
 	<p>Loading ...</p>
 {:then costumes}
 	<ul>
 		{#each costumes as costume}
-			<li><a href="/admin/costume/{costume._id}">Trang phục {costume.name}</a></li>
+			<li class="my-3">
+				<i class="fa-solid fa-arrow-right-to-bracket" />
+				<a href="/admin/costume/{costume._id}" class="costume-link">Trang phục {costume.name}</a>
+			</li>
 		{/each}
 	</ul>
 {:catch error}
@@ -89,13 +103,33 @@
 
 <style>
 	#new-costume-form {
-		flex: 1;
 		display: flex;
 		flex-direction: column;
 		padding: 1rem;
 		width: 100%;
-		max-width: 64rem;
 		margin: 0 auto;
-		box-sizing: border-box;
+	}
+	.text-input {
+		@apply my-1 rounded;
+		border: 1px solid #444654;
+		width: 30%;
+		height: 35px;
+	}
+	button {
+		@apply bg-lime-400 p-3 w-max my-4;
+		border-radius: 8px;
+	}
+	.costume-link {
+		box-shadow: inset 0 0 0 0 rgb(163, 230, 53);
+		color: #000;
+		margin: 0 -0.25rem;
+		padding: 0 0.25rem;
+		transition: color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+	}
+	.costume-link:hover {
+		@apply py-2;
+		box-shadow: inset 300px 0 0 0 rgb(163, 230, 53);
+		color: #000;
+		border-radius: 8px;
 	}
 </style>
--- src/routes/api/costume/+server.js
+++ src/routes/api/costume/+server.js
@@ -4,7 +4,7 @@ import Costume from 'base/models/Costume';
 /**
  * @type {import('./$types').RequestHandler}
  */
-export async function GET({ request }) {
+export async function GET() {
 	const costumes = await Costume.find().lean();
 	return json({ costumes });
 }
