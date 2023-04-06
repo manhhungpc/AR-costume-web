484ca17b4f9cb6fb74d538080013d6e3f8447412
manhhungpc <phamhung2002@gmail.com>
2023-04-06 23:59:00 +0700

    load admin costume data

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,10 +1,22 @@
 import mongoose from 'mongoose';
-import { DB_CONNECT } from '$env/static/private';
+import { v2 as cloudinary } from 'cloudinary';
+import {
+	CLOULDINARY_API_KEY,
+	CLOULDINARY_API_SECRET,
+	CLOULDINARY_NAME,
+	DB_CONNECT
+} from '$env/static/private';
 
 try {
 	// @ts-ignore
 	await mongoose.connect(DB_CONNECT);
 	console.log('Connected to mongo');
+
+	cloudinary.config({
+		cloud_name: CLOULDINARY_NAME,
+		api_key: CLOULDINARY_API_KEY,
+		api_secret: CLOULDINARY_API_SECRET
+	});
 } catch (err) {
 	console.log(err);
 }
--- src/routes/api/admin/costume/+server.js
+++ src/routes/api/admin/costume/+server.js
@@ -1,12 +1,28 @@
-// @ts-nocheck
 import { json } from '@sveltejs/kit';
 import { verify } from '$lib/server/token.js';
 import Costume from 'base/models/Costume';
 import { JWT_SECRET } from '$env/static/private';
+import { v2 as cloudinary } from 'cloudinary';
+
+/**
+ * @type {import('./$types').RequestHandler}
+ */
+export async function GET({ request }) {
+	// @ts-ignore
+	const token = request.headers.get('authorization').split(' ')[1];
+	if (!verify(token, JWT_SECRET)) {
+		return json({ status: 403, err: 'Không có quyền truy cập' });
+	}
+
+	const costumes = await Costume.find().lean();
+	return json({ costumes });
+}
+
 /**
  * @type {import('./$types').RequestHandler}
  */
 export async function POST({ request }) {
+	// @ts-ignore
 	const token = request.headers.get('authorization').split(' ')[1];
 	if (!verify(token, JWT_SECRET)) {
 		return json({ status: 403, err: 'Không có quyền truy cập' });
@@ -15,11 +31,22 @@ export async function POST({ request }) {
 	if (!req.name || !req.qr_image) {
 		return json({ status: 400, err: 'Thiếu thông tin' });
 	}
+	// console.log(req, req.qr_image);
+	const imgName = encodeURIComponent(req.name);
 
+	const uploadImg = await cloudinary.uploader.upload(req.qr_image, { public_id: imgName });
+	console.log('secure_url', uploadImg.secure_url);
+
+	const url = cloudinary.url(imgName, {
+		width: 300,
+		height: 300,
+		Crop: 'fill'
+	});
 	const data = {
 		name: req.name,
 		description: req.description || 'Không có mô tả',
-		qr: 'string',
+		qr: uploadImg.secure_url,
+		qr_gen_url: url,
 		url_3d: ''
 	};
 	const newCostume = await Costume.create(data);
